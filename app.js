2eaea0ae3264418da94f4fa57ff7eda6a8ca7fa8
kimmina <dir10@nate.com>
2023-05-31 16:10:38 +0900

    dd

--- app.js
+++ app.js
@@ -5,37 +5,30 @@ let web3;
 let contract;
 
 window.onload = function() {
+    document.getElementById('connectButton').addEventListener('click', connectMetamask);
     fetch('./IUP_ABI.json')
         .then(response => response.json())
         .then(data => {
             contractABI = data;
-            initializeWeb3();
         })
         .catch(error => console.error('Error:', error));
 };
 
-async function initializeWeb3() {
-    // Setting up Web3
+async function connectMetamask() {
     if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
         try {
-            // Request account access if needed
-            await window.ethereum.enable();
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            web3 = new Web3(window.ethereum);
             accounts = await web3.eth.getAccounts();
             setupContract();
+            document.getElementById('connectButton').style.display = 'none';
+            document.getElementById('lockedTokenDiv').style.display = 'block';
+            displayAccount();
         } catch (error) {
             console.error(error);
         }
-    }
-    // Legacy dapp browsers
-    else if (window.web3) {
-        web3 = new Web3(web3.currentProvider);
-        // Acccounts always exposed
-        setupContract();
-    }
-    // Non-dapp browsers
-    else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    } else {
+        console.log('Metamask not detected. Please install Metamask!');
     }
 }
 
@@ -46,12 +39,15 @@ function setupContract() {
 }
 
 function startApp() {
-    document.getElementById('connectButton').style.display = 'none';
-    document.getElementById('lockedTokenDiv').style.display = 'block';
-
     contract.methods.LockTimeOf(accounts[0]).call()
         .then(result => {
             const lockTime = new Date(result * 1000);
             document.getElementById('lockTime').innerText = 'Lock Time: ' + lockTime.toLocaleString();
         });
+}
+
+function displayAccount() {
+    const accountAddress = accounts[0];
+    const accountElement = document.getElementById('account');
+    accountElement.innerText = 'Account: ' + accountAddress;
 }
